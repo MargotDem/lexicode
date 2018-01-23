34d1bd854c9dc252b7ae3f0eaf936da145cb71fe
MargotDem <margot_demaulmont@hotmail.fr>
2018-01-23 20:39:45 +0100

    dryified EntriesContainer

--- src/components/EntriesContainer.js
+++ src/components/EntriesContainer.js
@@ -10,8 +10,62 @@ class EntriesContainer extends BaseContainer {
     this.refs[i].scrollIntoView({block: 'end', behavior: 'smooth'})
   }
 
+  fetchEntries (t, key, entriesNames, entriesArray) {
+    var title = entriesNames[key]
+
+    // retrieve the text, format it, and store it in the text variable
+
+    var unformattedText = t(title)
+
+    var text = unformattedText.split('///')
+
+    // links contains (jsx expressions) anchor elements with the link(s) for the given entry:
+
+    var links = []
+
+    for (var link in data[title]['links']) {
+      var linkAddress = data[title]['links'][link]['link']
+
+      var linkName = data[title]['links'][link]['name']
+
+      links.push(
+        <div>
+          <a target='_blank' href={linkAddress}>{linkName}</a>
+          <br />
+        </div>
+      )
+    }
+
+    // tags contains the tag(s) for the given entry:
+
+    var tags = []
+
+    for (var tag in data[title]['tags']) {
+      var tagName = data[title]['tags'][tag]
+      tags.push(tagName)
+    }
+
+    /*
+    finally pass the data of given entry to an Entry instance
+    (along with the Main component's state changing searchByTag function passed
+    as props under the name of onClick), and store the latter in entriesArray:
+    */
+
+    var entryComponent = (
+      <Entry
+        title={title}
+        text={text}
+        links={links}
+        tags={tags}
+        onClick={this.props.onClick}
+      />
+    )
+
+    entriesArray.push(entryComponent)
+  }
+
   renderMe (t) {
-         // entriesArray is going to receive every entry to be displayed as an Entry component:
+    // entriesArray is going to receive every entry to be displayed as an Entry component:
 
     var entriesArray = []
 
@@ -24,119 +78,28 @@ class EntriesContainer extends BaseContainer {
     // if no tags-bar__button has been clicked, display all entries:
 
     if (this.props.displayAll) {
-      // for each entry, pass its data to an Entry component that's stored in entriesArray:
+      /* for each entry, fetchEntries is going to pass the data to an
+      Entry component that's going to be stored in entriesArray:
+      */
 
       for (var key in entriesNames) {
-        var title = entriesNames[key]
-
-        // retrieve the text, format it, and store it in the text variable
-
-        var unformattedText = t(title)
-
-        var text = unformattedText.split('///')
-
-        // links contains (jsx expressions) anchor elements with the link(s) for the given entry:
-
-        var links = []
-
-        for (var link in data[title]['links']) {
-          var linkAddress = data[title]['links'][link]['link']
-
-          var linkName = data[title]['links'][link]['name']
-
-          links.push(
-            <div>
-              <a target='_blank' href={linkAddress}>{linkName}</a>
-              <br />
-            </div>
-          )
-        }
-
-        // tags contains the tag(s) for the given entry:
-
-        var tags = []
-
-        for (var tag in data[title]['tags']) {
-          var tagName = data[title]['tags'][tag]
-          tags.push(tagName)
-        }
-
-        // finally pass the data of given entry to an Entry instance (along with the Main component's state changing searchByTag function passed as props under the name of onClick), and store the latter in entriesArray:
-
-        var entryComponent = (
-          <Entry
-            title={title}
-            text={text}
-            links={links}
-            tags={tags}
-            onClick={this.props.onClick}
-          />
-        )
-
-        entriesArray.push(entryComponent)
+        this.fetchEntries(t, key, entriesNames, entriesArray)
       }
 
-      return (
-        <div className='entries'>
-          {entriesArray}
-        </div>
-      )
-
-        // if a tags-bar__button has been clicked, display every entry corresponding to said tag (pretty much the same thing, some refactoring to be done here):
+    // if a tags-bar__button has been clicked, display every entry corresponding to said tag:
     } else {
       for (var key in entriesNames) {
         if (data[entriesNames[key]]['tags'].indexOf(this.props.tagToDisplay) > -1) {
-          var title = entriesNames[key]
-
-          // retrieve the text, format it, and store it in the text variable
-
-          var unformattedText = t(title)
-
-          var text = unformattedText.split('///')
-
-          var links = []
-
-          for (var link in data[title]['links']) {
-            var linkAddress = data[title]['links'][link]['link']
-
-            var linkName = data[title]['links'][link]['name']
-
-            links.push(
-              <div>
-                <a target='_blank' href={linkAddress}>{linkName}</a>
-                <br />
-              </div>
-            )
-          }
-
-          var tags = []
-
-          for (var tag in data[title]['tags']) {
-            var tagName = data[title]['tags'][tag]
-            tags.push(tagName)
-          }
-
-          var entryCompo = (
-            <Entry
-              ref={key}
-              title={title}
-              text={text}
-              links={links}
-              tags={tags}
-              onClick={this.props.onClick}
-            />
-          )
-
-          entriesArray.push(entryCompo)
+          this.fetchEntries(t, key, entriesNames, entriesArray)
         }
       }
-
-      return (
-        <div className='entries'>
-          {entriesArray}
-        </div>
-      )
     }
+
+    return (
+      <div className='entries'>
+        {entriesArray}
+      </div>
+    )
   }
 }
 
