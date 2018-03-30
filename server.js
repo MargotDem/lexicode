56429b28c25e1a5b6fe2f799545ca32d97ca1338
MargotDem <margot_demaulmont@hotmail.fr>
2018-03-30 09:55:44 +0200

    suppressed unused code

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.get('/api/entries', (req, res) => {
   res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
   con.connect(function (err) {
     // getting rid of this makes the disturbing error disappear ha ha ha what is life
+    // edit fri 30/03 weirdd today the request wouldnt work and then i uncommented this
+    // and then it worked and then i recommented it and it still works and is this linked
+    // or not at all wth
     // if (err) throw err
     con.query('SELECT * FROM entries ORDER BY name', function (err, result) {
       if (err) throw err
