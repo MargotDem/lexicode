560b53e7efd9371078db19f17ef621d3cf7b8f6a
MargotDem <margot_demaulmont@hotmail.fr>
2018-04-05 13:13:21 +0200

    toggle language button rewrite

--- client/src/components/ToggleLanguage.js
+++ client/src/components/ToggleLanguage.js
@@ -3,27 +3,14 @@ import React, { Component } from 'react'
 import './styles/togglelanguage.css'
 
 export default class ToggleLanguage extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      en: true
-    }
-    this.changeLanguage = this.changeLanguage.bind(this)
-  }
-
-  changeLanguage (language) {
-    this.props.changeLanguage(language)
-  }
-
   render () {
-    let isFr = this.props.language !== 'en' ? ' toggle-language__button_on' : ''
-    let isEn = this.props.language === 'en' ? ' toggle-language__button_on' : ''
+    let { language, changeLanguage } = this.props
     return (
       <div className='toggle-language'>
-        <div className={'toggle-language__button' + isEn} onClick={() => { this.changeLanguage('en') }}>
+        <div className={'toggle-language__button ' + (language === 'en' && 'toggle-language__button_on')} onClick={() => { changeLanguage('en') }}>
           EN
         </div>
-        <div className={'toggle-language__button' + isFr} onClick={() => { this.changeLanguage('fr') }}>
+        <div className={'toggle-language__button ' + (language === 'fr' && 'toggle-language__button_on')} onClick={() => { changeLanguage('fr') }}>
           FR
         </div>
       </div>
